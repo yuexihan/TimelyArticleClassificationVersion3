12e8992d93f5e254b8fa95292b76856977060b93
leonyue <leonyue@tencent.com>
2018-06-13 20:29:07 +0800

    fix size

--- train_test_data.py
+++ train_test_data.py
@@ -1,6 +1,6 @@
 import random
-from glob import glob
 from datetime import date
+from tqdm import tqdm
 
 f_p = open('data/positive.train', 'w', encoding='utf-8')
 f_n = open('data/negative.train', 'w', encoding='utf-8')
@@ -10,16 +10,17 @@ for day in range(2, 12):
     day = day.strftime('%Y%m%d')
     file = 'data/%s.positive' % day
     ids = []
-    for line in open(file, encoding='utf-8'):
+    for line in tqdm(open(file, encoding='utf-8')):
         inner_id, rest = line.split('\t', 1)
         if len(line.split()) < 50:
             continue
         ids.append(inner_id)
     random.shuffle(ids)
     ids = set(ids[:10000])
-    for line in open(file, encoding='utf-8'):
+    for line in tqdm(open(file, encoding='utf-8')):
         inner_id, rest = line.split('\t', 1)
         if inner_id in ids:
+            ids.remove(inner_id)
             f_p.write(line)
 
 
@@ -28,14 +29,15 @@ for day in range(2, 12):
     day = day.strftime('%Y%m%d')
     file = 'data/%s.negative' % day
     ids = []
-    for line in open(file, encoding='utf-8'):
+    for line in tqdm(open(file, encoding='utf-8')):
         inner_id, rest = line.split('\t', 1)
         if len(line.split()) < 50:
             continue
         ids.append(inner_id)
     random.shuffle(ids)
     ids = set(ids[:10000])
-    for line in open(file, encoding='utf-8'):
+    for line in tqdm(open(file, encoding='utf-8')):
         inner_id, rest = line.split('\t', 1)
         if inner_id in ids:
+            ids.remove(inner_id)
             f_n.write(line)
