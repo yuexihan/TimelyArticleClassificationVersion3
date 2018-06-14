c855f1a60232fc2645bcec65b8d87ac478bca262
leonyue <leonyue@tencent.com>
2018-06-14 11:33:55 +0800

    fix bug

--- badcase.py
+++ badcase.py
@@ -10,7 +10,7 @@ FLAGS = flags.FLAGS
 
 f = open(FLAGS.badcase, 'w', encoding='utf-8')
 i = 0
-for a, b in zip(itertools.chain(open('data/20180612.positve', encoding='utf-8'), open('data/20180612.negative', encoding='utf-8')), open(FLAGS.predictions, encoding='utf-8')):
+for a, b in zip(itertools.chain(open('data/20180612.positive', encoding='utf-8'), open('data/20180612.negative', encoding='utf-8')), open(FLAGS.predictions, encoding='utf-8')):
     if i < 34431 and float(b) < 0 or i >= 34431 and float(b) > 0:
         f.write(a)
         f.write(b)
